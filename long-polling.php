7afff085f31de99f2d4bc6b0f861e537433b3312
Gérley Adriano <gerleyadriano7@gmail.com>
2016-08-16 18:08:26 -0300

    Set Bot Token Information

--- long-polling.php
+++ long-polling.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'DebianBot.php';
 
-define('BOT_TOKEN', '257666118:AAGnC4fEN2dABmtpxAgNxnTqj054YXh-u9Y');
+define('BOT_TOKEN', 'your bot token');
 
 $bot = new DebianDayBot(BOT_TOKEN, 'DebianDayBotChat');
 $bot->runLongpoll();
